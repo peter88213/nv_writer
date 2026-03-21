fc4bed09b3c3b8b9149db994ff8970aeaa706f94
peter88213 <petert88213@gmail.com>
2026-03-21 10:51:53 +0100

    Refactored FooterBar.

--- src/nvwriter/footer_bar.py
+++ src/nvwriter/footer_bar.py
@@ -13,119 +13,66 @@ import tkinter as tk
 class FooterBar(tk.Frame):
 
     def __init__(self, parent, **kw):
-        super().__init__(
-            parent,
-            background=prefs['color_bg'],
-            **kw,
-        )
-
-        #--- Add buttons to the bottom line.
-        self._entries = []
-
-        #--- Create new section.
-        text, accelerator = self._create_menu_entry(
-            _('New'),
-            KEYS.CREATE_SECTION[1],
-            '<<new_section>>'
-        )
-        self._entries.append((text, accelerator))
-
-        #--- Split section at cursor position.
-        text, accelerator = self._create_menu_entry(
-            _('Split'),
-            KEYS.SPLIT_SECTION[1],
-            '<<split_section>>'
-        )
-        self._entries.append((text, accelerator))
-
-        #--- Clone section.
-        text, accelerator = self._create_menu_entry(
-            _('Clone'),
-            KEYS.CLONE_SECTION[1],
-            '<<clone_section>>'
-        )
-        self._entries.append((text, accelerator))
-
-        #--- Previous.
-        text, accelerator = self._create_menu_entry(
-            _('Previous'),
-            KEYS.PREVIOUS[1],
-            '<<load_prev>>'
-        )
-        self._entries.append((text, accelerator))
-
-        #--- Next.
-        text, accelerator = self._create_menu_entry(
-            _('Next'),
-            KEYS.NEXT[1],
-            '<<load_next>>'
-        )
-        self._entries.append((text, accelerator))
-
-        #--- Save.
-        text, accelerator = self._create_menu_entry(
-            _('Save'),
-            KEYS.SAVE[1],
-            '<<save>>'
-        )
-
-        self._entries.append((text, accelerator))
-        #--- Close.
-        text, accelerator = self._create_menu_entry(
-            _('Close'),
-            KEYS.END_WRITING_MODE[1],
-            '<<on_quit>>'
-        )
-        self._entries.append((text, accelerator))
-
-    def _create_menu_entry(self, text, accelerator, sequence):
 
         def callback(*_):
             root = self.master.winfo_toplevel()
             root.event_generate(sequence)
 
-        event = callback
-        entry = tk.Frame(
-            self,
-            background=prefs['color_bg'],
-            padx=1,
-            pady=1,
-        )
-        text = tk.Label(
-            entry,
-            background=prefs['color_button_bg'],
-            foreground=prefs['color_button_fg'],
-            text=text,
-        )
-        text.pack(
-            fill='x',
-            expand=True,
-        )
-        text.bind('<Button-1>', event)
-        accelerator = tk.Label(
-            entry,
-            background=prefs['color_bg'],
-            foreground=prefs['color_shortcut'],
-            text=accelerator,
-        )
-        accelerator.pack(
-            fill='x',
-            expand=True,
-        )
-        accelerator.bind('<Button-1>', event)
-        entry.pack(
-            side='left',
-            padx=4,
-            pady=2,
-            fill='x',
-            expand=True,
-        )
-        return text, accelerator
+        super().__init__(parent, background=prefs['color_bg'], **kw,)
+
+        #--- Add buttons to the bottom line.
+        self._entries = []
+        for desc, shortcut, sequence in (
+            (_('New'), KEYS.CREATE_SECTION[1], '<<new_section>>',),
+            (_('Split'), KEYS.SPLIT_SECTION[1], '<<split_section>>',),
+            (_('Clone'), KEYS.CLONE_SECTION[1], '<<clone_section>>',),
+            (_('Previous'), KEYS.PREVIOUS[1], '<<load_prev>>',),
+            (_('Next'), KEYS.NEXT[1], '<<load_next>>',),
+            (_('Save'), KEYS.SAVE[1], '<<save>>',),
+            (_('Close'), KEYS.END_WRITING_MODE[1], '<<on_quit>>',),
+        ):
+            event = callback
+            menuEntry = tk.Frame(
+                self,
+                background=prefs['color_bg'],
+                padx=1,
+                pady=1,
+            )
+            descLabel = tk.Label(
+                menuEntry,
+                background=prefs['color_button_bg'],
+                foreground=prefs['color_button_fg'],
+                text=desc,
+            )
+            descLabel.pack(
+                fill='x',
+                expand=True,
+            )
+            descLabel.bind('<Button-1>', event)
+            shortcutLabel = tk.Label(
+                menuEntry,
+                background=prefs['color_bg'],
+                foreground=prefs['color_shortcut'],
+                text=shortcut,
+            )
+            shortcutLabel.pack(
+                fill='x',
+                expand=True,
+            )
+            shortcutLabel.bind('<Button-1>', event)
+            menuEntry.pack(
+                side='left',
+                padx=4,
+                pady=2,
+                fill='x',
+                expand=True,
+            )
+            self._entries.append((descLabel, shortcutLabel))
 
     def set_font(self, scale):
         size = int(int(prefs['font_size_1']) * scale * 0.8)
         font = (prefs['editor_font'], size)
-        for text, accelerator in self._entries:
-            text.configure(font=font)
-            accelerator.configure(font=font)
+        for descLabel, shortcutLabel in self._entries:
+            descLabel.configure(font=font)
+            shortcutLabel.configure(font=font)
 
