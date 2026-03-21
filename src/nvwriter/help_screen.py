f52efd964fee5c82da0e92b55ac5221a413cce7e
peter88213 <petert88213@gmail.com>
2026-03-21 11:39:21 +0100

    Refactored HelpScreen (#9).

--- src/nvwriter/help_screen.py
+++ src/nvwriter/help_screen.py
@@ -13,124 +13,56 @@ import tkinter as tk
 class HelpScreen(tk.Frame):
 
     def __init__(self, parent, **kw):
-        super().__init__(
-            parent,
-            bg=prefs['color_bg'],
-        )
-        outerFrame = tk.Frame(
-            self,
-            padx=2,
-            pady=2,
-            bg=prefs['color_fg'],
-        )
-        outerFrame.pack(
-            padx=6,
-            pady=6,
-            fill='both',
-        )
+        super().__init__(parent, bg=prefs['color_bg'],)
+        outerFrame = tk.Frame(self, padx=2, pady=2, bg=prefs['color_fg'],)
+        outerFrame.pack(padx=6, pady=6, fill='both',)
         self._innerFrame = tk.Frame(
-            outerFrame,
-            padx=6,
-            pady=6,
-            bg=prefs['color_bg'],
+            outerFrame, padx=6, pady=6, bg=prefs['color_bg'],
         )
-        self._innerFrame.pack(
-            fill='both',
-        )
+        self._innerFrame.pack(fill='both',)
+        self._headings = []
         self._entries = []
         row = 0
         col = 0
-        #--- Create new section.
-        desc, shortcut = self._create_help_entry(
-            _('New'),
-            KEYS.CREATE_SECTION[1],
-            row=row,
-            col=col,
+
+        self._headings.append(
+            self._create_heading(_('Section'), row, col)
         )
-        self._entries.append((desc, shortcut))
+        for desc, shortcut in (
+            (_('New'), KEYS.CREATE_SECTION[1],),
+            (_('Split'), KEYS.SPLIT_SECTION[1],),
+            (_('Clone'), KEYS.CLONE_SECTION[1],),
+            (_('Previous'), KEYS.PREVIOUS[1],),
+            (_('Next'), KEYS.NEXT[1],),
+            (_('Save'), KEYS.SAVE[1],),
+            (_('Close'), KEYS.END_WRITING_MODE[1],),
+        ):
+            row += 1
+            self._entries.append(
+                self._create_help_entry(desc, shortcut, row, col)
+            )
 
-        #--- Split section at cursor position.
-        row = 1
-        col = 0
-        desc, shortcut = self._create_help_entry(
-            _('Split'),
-            KEYS.SPLIT_SECTION[1],
-            row=row,
-            col=col,
-        )
-        self._entries.append((desc, shortcut))
+    def _create_heading(self, heading, row, col):
 
-        #--- Clone section.
-        row = 2
-        col = 0
-        desc, shortcut = self._create_help_entry(
-            _('Clone'),
-            KEYS.CLONE_SECTION[1],
-            row=row,
-            col=col,
-        )
-        self._entries.append((desc, shortcut))
-
-        #--- Previous.
-        row = 0
-        col = 2
-        desc, shortcut = self._create_help_entry(
-            _('Previous'),
-            KEYS.PREVIOUS[1],
-            row=row,
-            col=col,
-        )
-        self._entries.append((desc, shortcut))
-
-        #--- Next.
-        row = 1
-        col = 2
-        desc, shortcut = self._create_help_entry(
-            _('Next'),
-            KEYS.NEXT[1],
-            row=row,
-            col=col,
-        )
-        self._entries.append((desc, shortcut))
-
-        row = 2
-        col = 2
-        #--- Save.
-        desc, shortcut = self._create_help_entry(
-            _('Save'),
-            KEYS.SAVE[1],
-            row=row,
-            col=col,
-        )
-
-        row = 0
-        col = 4
-        self._entries.append((desc, shortcut))
-        #--- Close.
-        desc, shortcut = self._create_help_entry(
-            _('Close'),
-            KEYS.END_WRITING_MODE[1],
-            row=row,
-            col=col,
-        )
-        self._entries.append((desc, shortcut))
-
-    def _create_help_entry(self, desc, shortcut, row, col):
-
-        shortcut = tk.Label(
+        headingLabel = tk.Label(
             self._innerFrame,
             background=prefs['color_bg'],
-            foreground=prefs['color_fg'],
-            text=shortcut,
+            foreground=prefs['color_shortcut'],
+            text=heading,
             anchor='w',
             padx=10,
         )
-        shortcut.grid(
+        headingLabel.grid(
             row=row,
             column=col,
+            columnspan=2,
             sticky="ew",
         )
-        desc = tk.Label(
+        return headingLabel
+
+    def _create_help_entry(self, desc, shortcut, row, col):
+
+        descLabel = tk.Label(
             self._innerFrame,
             background=prefs['color_bg'],
             foreground=prefs['color_fg'],
@@ -138,18 +70,34 @@ class HelpScreen(tk.Frame):
             anchor='w',
             padx=10,
         )
-        desc.grid(
+        descLabel.grid(
+            row=row,
+            column=col,
+            sticky="ew",
+        )
+        shortcutLabel = tk.Label(
+            self._innerFrame,
+            background=prefs['color_bg'],
+            foreground=prefs['color_fg'],
+            text=shortcut,
+            anchor='w',
+            padx=10,
+        )
+        shortcutLabel.grid(
             row=row,
             column=col + 1,
             sticky="ew",
         )
-        return desc, shortcut
+        return descLabel, shortcutLabel
 
     def set_font(self, scale):
-        size = int(int(prefs['font_size_1']) * scale * 0.8)
+        size = int(int(prefs['font_size_1']) * scale)
         accFont = (prefs['editor_font'], size, 'bold')
         dscFont = (prefs['editor_font'], size)
-        for desc, shortcut in self._entries:
-            shortcut.configure(font=accFont)
-            desc.configure(font=dscFont)
+        for descLabel, shortcutLabel in self._entries:
+            shortcutLabel.configure(font=accFont)
+            descLabel.configure(font=dscFont)
+        hdFont = (prefs['editor_font'], size, 'bold')
+        for headinglabel in self._headings:
+            headinglabel.configure(font=hdFont)
 
